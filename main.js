7f2fd0de631620a0ffe5f66519ad5f7a18a7e8d1
ikourinai <110175676+ikourinai@users.noreply.github.com>
2024-02-02 00:12:50 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-'use strict';
-
-// ヒーローエリア //
-{
-  function play() {
-    setTimeout(() => {
-      images[currentIndex].classList.remove('current');
-      currentIndex++;
-      if (currentIndex > images.length - 1) {
-        currentIndex = 0;
-      }
-      images[currentIndex].classList.add('current');
-      play();
-    }, 5000);
-    
-  }
-  
-  const images = document.querySelectorAll('.hero-img img');
-  let currentIndex = 0;  //現在表示している画像が何番目か。最初は０番目
-  
-  play();
-
-}
-
-// お知らせ //
-    
 const dts = document.querySelectorAll('dt');
     
 //dtをクリックしたら親要素のdivにappearクラスを付け外しする
